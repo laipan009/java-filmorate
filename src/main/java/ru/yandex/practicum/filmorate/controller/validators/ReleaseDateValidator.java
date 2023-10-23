f793623b5524c995df1766cd9d7748a3bc6c2709
Arsen <laipan09@mail.ru>
2023-10-23 11:41:03 +0300

    final for pom

--- src/main/java/ru/yandex/practicum/filmorate/controller/validators/ReleaseDateValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/validators/ReleaseDateValidator.java
@@ -1,7 +1,7 @@
 package ru.yandex.practicum.filmorate.controller.validators;
-import jakarta.validation.constraints.*;
-import jakarta.validation.ConstraintValidator;
-import jakarta.validation.ConstraintValidatorContext;
+import javax.validation.constraints.*;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
 import java.time.LocalDate;
 
 public class ReleaseDateValidator implements ConstraintValidator<ValidReleaseDate, LocalDate> {
--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,9 +1,9 @@
 package ru.yandex.practicum.filmorate.model;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 import lombok.Data;
 import ru.yandex.practicum.filmorate.controller.validators.ValidReleaseDate;
 
